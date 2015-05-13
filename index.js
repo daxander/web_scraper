85dd8e259bc7559cfa79863a0eba134adfe76349
David Feimer <davidfeimer@gmail.com>
2015-05-13 18:25:25 +0000

    add fast csv and update index.js

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 
 
-
+var cheerio = require("cheerio");
 var request = require('request');
+var csv = require('fast-csv');
+var path = require('path');
+var fs = require('fs')
+
+
+var csvStream = csv.createWriteStream({headers: true}),
+    writableStream = fs.createWriteStream("images.csv");
+ 
+writableStream.on("finish", function(){
+  console.log("DONE!");
+});
+
 request('http://substack.net/images/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+    var $ = cheerio.load(body);
+    csvStream.pipe(writableStream);
+    $('tr').each(function(i, element) {
+      var url = 'http://substack.net/images' + $(this).find('a').attr('href');
+      var permissions = $(this).children().first().text();
+      var type = path.extname(url);
+      var arr = [permissions,url,type];
+
+      csvStream.write(arr);
+
+    });
+    csvStream.end();
+
   }
 })
+
